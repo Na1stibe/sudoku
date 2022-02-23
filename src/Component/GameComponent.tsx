da9264d0edc37b3ce302456a2fdbdc8c5604c4d2
nawonseog <fheoa700000@naver.com>
2022-02-24 04:22:47 +0900

    2일차 알고리즘이 조금 어렵다... 결국 https://im-developer.tistory.com/43 를 참고하자..

--- src/Component/GameComponent.tsx
+++ src/Component/GameComponent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from "styled-components";
 import SquareListComponent from "./Square/SquareListComponent";
 
@@ -14,6 +14,51 @@ const Container = styled.div`
 `
 
 const GameComponent = () => {
+  const [sudokuNumberList, setSudokuNumberList] = useState<number[]>([])
+  console.log('asd')
+  useEffect(() => {
+    makeSudokuGameNumber()
+  }, [])
+
+  const makeSudokuGameNumber = () => {
+    for (let square = 0; square < 9; square++) {
+      for (let number = 0; number < 9; number++) {
+        createNumber(square, number)
+      }
+    }
+  }
+
+  const createNumber = (squareCount: number, number: number) => {
+    let count: number = 0
+    while (count > 9) {
+      let itemNumber: number = getRandomArbitrary(1, 9)
+      deduplicationSquareNumber(squareCount, number, itemNumber)
+    }
+  }
+
+  const deduplicationSquareNumber = (squareCount: number, number: number, itemNumber: number) => {
+    switch (squareCount) {
+      case 0 :
+        return sudokuNumberList.push(itemNumber)
+      case 1:
+        return
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+      case 7:
+      case 8:
+    }
+  }
+
+  const deduplicationSudokuNumber = (number: number, itemNumber: number) => {
+
+  }
+
+  const getRandomArbitrary = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
   return (
     <Container>
       <SquareListComponent/>
